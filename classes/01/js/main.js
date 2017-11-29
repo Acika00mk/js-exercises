023e64a2afe48cd8706968447a3dfa39fa3dc2f9
aleks-inside-mk <aleksandar.kostadinovski@inside-development.mk>
2017-11-29 18:28:28 +0100

    console stuff

--- classes/01/js/main.js
+++ classes/01/js/main.js
@@ -1,1 +1,16 @@
+// inline comment
+
+
+/*
+console.log('Zdravo');
+console.log('pred debugger;');
+
+
 debugger;
+
+console.log("debugg");
+
+*/
+
+alert("asd adsf asdf aisdfn uaskdf");
+console.log('da');
