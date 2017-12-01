41e7c6164770babe5b6f22e35f9b51aebb40a956
aleks-inside-mk <aleksandar.kostadinovski@inside-development.mk>
2017-12-01 18:52:32 +0100

    input click over

--- classes/02/js/main.js
+++ classes/02/js/main.js
@@ -1,7 +1,27 @@
-function printer(kolkuPati) {
-    if (parseInt(kolkuPati)) {
-        for (var i = 0; i < kolkuPati; i++) {
-            console.log(i);
-        }
-    }
+function elementSelector (element) {
+    var selectedElement =
+        document.getElementById(element);
+
+    second(selectedElement);
+}
+
+function second(element){
+    console.log(element);
+}
+function zdravo (){
+    console.log("zdravo");
+}
+
+function cao (){
+    console.log("cao");
+}
+
+function klikniMe(){
+    console.log("falaaaaa");
+}
+
+
+function kopce(event){
+
+    console.log(event.key);
 }
