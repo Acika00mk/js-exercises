dd899d8c8739a68edfc8d9736fb6cc02c937bb68
aleks-inside-mk <aleksandar.kostadinovski@inside-development.mk>
2017-12-01 17:59:45 +0100

    functions

---
+++ classes/02/js/main.js
@@ -0,0 +1,15 @@
+
+
+
+
+function callMeMaybe(name) {
+    console.log(name)
+    for (var i = 0; i < 10; i++) {
+        console.log("da" + i);
+    }
+}
+
+callMeMaybe()
+callMeMaybe("asdasd")
+callMeMaybe(213)
+callMeMaybe(213)
