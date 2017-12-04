907b9e0784a514833ac58c6d436c80cf46d7fe12
aleks-inside-mk <aleksandar.kostadinovski@inside-development.mk>
2017-12-04 19:50:31 +0100

    jquery animate

--- classes/03/js/main.js
+++ classes/03/js/main.js
@@ -1,8 +1,29 @@
 $(function () {
 
-    $('#get').on('click', function () {
-        $.get('http://api.icndb.com/jokes/random?firstName=Ivan&lastName=Iscelitel', function (data) {
-            $('#joke').html(data.value.joke)
-        })
-    });
+    $('.kocka').on('click', function(){
+        alert("Bravo");
+    })
+
+    $("html").mousemove(function(e) {
+        var kocka = $('.kocka');
+        var kockaLeft = $(".kocka").offset().left;
+        var kockaTop = $(".kocka").offset().top;
+        var mouseLeft = e.clientX;
+        var mouseTop = e.clientY;
+
+
+        if( mouseLeft <  ( kockaLeft + 70) && mouseLeft> (kockaLeft + 50)   ){
+            kocka.animate({'left' : kockaLeft - 10 + 'px'}, 1)
+        }
+
+
+        if(mouseTop < (kockaTop + 70) && mouseTop > kockaTop + 50){
+            kocka.animate({'top' : kockaTop - 10 + 'px'}, 1)
+        }
+
+
+    console.log(mouseLeft, mouseTop, kockaLeft, kockaTop );
+})
+
+
 })
