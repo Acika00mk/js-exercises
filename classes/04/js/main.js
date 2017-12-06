6dcfef8ad9637d26f09cfbfc434f65cfdfbe9bbe
aleks-inside-mk <aleksandar.kostadinovski@inside-development.mk>
2017-12-06 18:46:10 +0100

    ajax

--- classes/04/js/main.js
+++ classes/04/js/main.js
@@ -1,4 +1,32 @@
 $(function () {
 
+    $('#get').on('click', function () {
+        var value = $('#input').val();
+
+        $.ajax('https://api.github.com/search/repositories', {
+            "type": "GET",
+            "data": {
+                "q": value.split(' ').join('+')
+            }
+        }).success(function (data) {
+            var elements = "";
+            console.log(data);
+
+            for (var i = 0; i < data.items.length - 1; i++) {
+                elements += '<li>' + data.items[i].name + '</li>';
+            }
+            $('#results').append(elements);
+        });
+
+        //        $.get('https://api.github.com/search/repositories', function (data) {
+        //            var elements = "";
+        //
+        //            for (var i = 0; i < data.total_count - 1; i++) {
+        //                elements += '<li>' + data.items[i].name + '</li>';
+        //            }
+        //            $('#results').append(elements);
+        //        })
+    })
+
 
 })
