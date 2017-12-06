08b9dfe7c2535da08a5cc570dcc0a37133f6c820
aleks-inside-mk <aleksandar.kostadinovski@inside-development.mk>
2017-12-06 19:15:08 +0100

    autocomplete

--- classes/04/js/main.js
+++ classes/04/js/main.js
@@ -1,32 +1,32 @@
 $(function () {
 
-    $('#get').on('click', function () {
-        var value = $('#input').val();
+    //        https://api.github.com/search/repositories
+var inProgress = false;
+    $('#input').on('keyup', function () {
+        var momentalnoKarakteri = this.value.length;
+        console.log(momentalnoKarakteri);
+        if(momentalnoKarakteri >= 3) {
 
-        $.ajax('https://api.github.com/search/repositories', {
-            "type": "GET",
-            "data": {
-                "q": value.split(' ').join('+')
+           if(!inProgress){
+               inProgress = true; $.get('https://api.github.com/search/repositories?q=' + this.value, function (data) {
+                console.log(data);
+                var elements = '';
+                for(var i = 0; i < 3; i++){
+                    elements += "<li reden-broj='"+i+"' class='item'>"+ data.items[i].name + "</li>"
+                }
+                $('#results').html("").append(elements);
+
+                $('.item').on('click', function(){
+                 $('#details').html(data.items[$(this).attr('reden-broj')].name)
+
+                })
+                inProgress = false ;
+            })
+        }
+            else {
+                alert('Vo progres e uste cekaj malce ')
             }
-        }).success(function (data) {
-            var elements = "";
-            console.log(data);
-
-            for (var i = 0; i < data.items.length - 1; i++) {
-                elements += '<li>' + data.items[i].name + '</li>';
-            }
-            $('#results').append(elements);
-        });
-
-        //        $.get('https://api.github.com/search/repositories', function (data) {
-        //            var elements = "";
-        //
-        //            for (var i = 0; i < data.total_count - 1; i++) {
-        //                elements += '<li>' + data.items[i].name + '</li>';
-        //            }
-        //            $('#results').append(elements);
-        //        })
+        }
     })
-
 
 })
