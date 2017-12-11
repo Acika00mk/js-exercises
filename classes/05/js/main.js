da08ae951570eb9f988f5d9107634ba02b41c8e3
aleks-inside-mk <aleksandar.kostadinovski@inside-development.mk>
2017-12-11 19:40:03 +0100

    drag and drop

--- classes/05/js/main.js
+++ classes/05/js/main.js
@@ -1,25 +1,52 @@
 $(function () {
-    var odLocalBrojcanik = localStorage.getItem('kliknato');
-    $('#klikovi').html(odLocalBrojcanik);
-
-    $('#add').on('click', function(){
-        var key = $('#key').val();
-        var value = $('#input').val();
-        localStorage.setItem( key, value);
-    })
-
-    $('#remove').on('click',function(){
-        var key = $('#key').val();
-        localStorage.removeItem(key);
-    })
-
-
-    $('#dont').on('click', function(){
-        odLocalBrojcanik = localStorage.getItem('kliknato');
-        odLocalBrojcanik++;
-        localStorage.setItem('kliknato', odLocalBrojcanik);
-        $('#klikovi').html(odLocalBrojcanik);
-    })
-
-
+    //    var odLocalBrojcanik = localStorage.getItem('kliknato');
+    //    $('#klikovi').html(odLocalBrojcanik);
+    //
+    //    $('#add').on('click', function(){
+    //        var key = $('#key').val();
+    //        var value = $('#input').val();
+    //        localStorage.setItem( key, value);
+    //    })
+    //
+    //    $('#remove').on('click',function(){
+    //        var key = $('#key').val();
+    //        localStorage.removeItem(key);
+    //    })
+    //
+    //
+    //    $('#dont').on('click', function(){
+    //        odLocalBrojcanik = localStorage.getItem('kliknato');
+    //        odLocalBrojcanik++;
+    //        localStorage.setItem('kliknato', odLocalBrojcanik);
+    //        $('#klikovi').html(odLocalBrojcanik);
+    //    })
 })
+
+var elementStoGoVlecam;
+
+function dragStart(event) {
+
+    console.log('dragggg');
+    console.log(event.target)
+    elementStoGoVlecam = event.target;
+}
+
+function dropped(event) {
+    event.preventDefault();
+    console.log('dropped');
+    $('.bazen').append($(elementStoGoVlecam))
+
+}
+
+function dragOver(event) {
+    event.preventDefault();
+    console.log('da');
+}
+
+function enter() {
+    $('.bazen').css('background', 'red');
+}
+
+function leave() {
+    $('.bazen').css('background', 'lightblue');
+}
