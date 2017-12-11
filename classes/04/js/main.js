cd53a36794ba4d716c4f2b4b084b991f566f657e
aleks-inside-mk <aleksandar.kostadinovski@inside-development.mk>
2017-12-11 18:05:17 +0100

    jquery

--- classes/04/js/main.js
+++ classes/04/js/main.js
@@ -1,32 +1,46 @@
 $(function () {
 
     //        https://api.github.com/search/repositories
-var inProgress = false;
-    $('#input').on('keyup', function () {
-        var momentalnoKarakteri = this.value.length;
-        console.log(momentalnoKarakteri);
-        if(momentalnoKarakteri >= 3) {
+//    $('#input').on('keyup', function () {
+//            var momentalnoKarakteri = this.value.length;
+//            console.log(momentalnoKarakteri);
+//            if (momentalnoKarakteri >= 3) {
+//                $.get('https://api.github.com/search/repositories?q=' + this.value, function (data) {
+//                    console.log(data);
+//                    var elements = '';
+//                    for (var i = 0; i < 3; i++) {
+//                        elements += "<li reden-broj='" + i + "' class='item'>" + data.items[i].name + "</li>"
+//                    }
+//                    $('#results').html("").append(elements);
+//
+//                    $('.item').on('click', function () {
+//                        $('#details').html(data.items[$(this).attr('reden-broj')].name)
+//
+//                    })
+//                })
+//            }
+//        }
+//    })
 
-           if(!inProgress){
-               inProgress = true; $.get('https://api.github.com/search/repositories?q=' + this.value, function (data) {
-                console.log(data);
-                var elements = '';
-                for(var i = 0; i < 3; i++){
-                    elements += "<li reden-broj='"+i+"' class='item'>"+ data.items[i].name + "</li>"
-                }
-                $('#results').html("").append(elements);
 
-                $('.item').on('click', function(){
-                 $('#details').html(data.items[$(this).attr('reden-broj')].name)
+    $('#search').on('click', function(){
+        var vrednost = $('#input').val();
+        console.log(vrednost);
 
-                })
-                inProgress = false ;
-            })
-        }
-            else {
-                alert('Vo progres e uste cekaj malce ')
-            }
-        }
+        $.get('https://api.github.com/search/repositories?q=' + vrednost, function(data){
+            console.log(data);
+        })
     })
+
+
+
+
+
+
+
+
+
+
+
 
 })
