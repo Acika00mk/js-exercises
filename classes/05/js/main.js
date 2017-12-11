08b204dbda06492f42628cd2699eb1808def3234
aleks-inside-mk <aleksandar.kostadinovski@inside-development.mk>
2017-12-11 19:12:14 +0100

    localStorage

--- classes/05/js/main.js
+++ classes/05/js/main.js
@@ -1,3 +1,25 @@
 $(function () {
+    var odLocalBrojcanik = localStorage.getItem('kliknato');
+    $('#klikovi').html(odLocalBrojcanik);
+
+    $('#add').on('click', function(){
+        var key = $('#key').val();
+        var value = $('#input').val();
+        localStorage.setItem( key, value);
+    })
+
+    $('#remove').on('click',function(){
+        var key = $('#key').val();
+        localStorage.removeItem(key);
+    })
+
+
+    $('#dont').on('click', function(){
+        odLocalBrojcanik = localStorage.getItem('kliknato');
+        odLocalBrojcanik++;
+        localStorage.setItem('kliknato', odLocalBrojcanik);
+        $('#klikovi').html(odLocalBrojcanik);
+    })
+
 
 })
