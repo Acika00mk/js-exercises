2f61c8ad3bd797a2448195fd1f72bf024a1102bf
aleks-inside-mk <aleksandar.kostadinovski@inside-development.mk>
2017-12-13 18:05:54 +0100

    MATRIX

--- classes/ships/js/main.js
+++ classes/ships/js/main.js
@@ -1,24 +1,14 @@
 $(function(){
-    var puzzles  = ['0-0' , '0-1', '0-2',
-                    '1-0', '1-1', '1-2',
-                    '2-0', '2-1', '2-2'];
-    var pieces = ""
-    function shuffleArray(array) {
-    for (var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
-}
-    var newArray = puzzles;
-    shuffleArray(newArray);
-
-
-    for(var i=0; i< 9; i++){
-        pieces += "<div class='piece image-"+newArray[i]+"'> </div>"
-    }
-    $('.puzzle figure').html("");
-    $('.puzzle figure').append(pieces);
+    var i, j = 0;
+    var niza = "";
+    for(i=0; i<10; i++)
+        {
+            for(j=0; j<10; j++)
+                {
+                niza+="<div class='pole'></div>";
+                }
+            niza+="<div class='clear'></div>"
+        }
+    $("#more").append(niza);
 
 })
